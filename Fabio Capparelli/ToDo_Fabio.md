cb03af08b127dd99989912b8b2d79e2be40b072a
Fafixxx96 <faficapparelli@gmail.com>
2021-07-31 17:17:21 +0200

    #8 last commit

---
+++ Fabio Capparelli/ToDo_Fabio.md
@@ -0,0 +1,1 @@
+Studying Spotify API and how to import, use it in Python, Scala and Java.
--- tests/test.py
+++
@@ -1,4 +0,0 @@
-import datetime
-
-old = datetime.datetime.now()
-dir1 = "/media/pi/Telecamere/Video/telecamere/previewName" + "/" + str(old.month) + "_" +  str(old.strftime("%b") + "-" + str(old.day) + "-" + str(old.year)
